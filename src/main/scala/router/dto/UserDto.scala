0df38cd9d1ebf51aac5c4c9a41b120ab18fcdfc8
Gneotux <gneotux@gmail.com>
2015-07-20 23:46:50 +0300

    Modified UserDto with new model

--- src/main/scala/router/dto/UserDto.scala
+++ src/main/scala/router/dto/UserDto.scala
@@ -12,15 +12,27 @@ case class UserDto(
   @(ApiModelProperty @field)(required = true, value = "email of the user")
   email: String,
 
-  @(ApiModelProperty @field)(value = "name of the user")
-  name: Option[String] = None,
+  @(ApiModelProperty @field)(value = "user's first name")
+  firstName: Option[String] = None,
 
-  @(ApiModelProperty @field)(value = "surname of the user")
-  surname: Option[String] = None,
+  @(ApiModelProperty @field)(value = "user's last name")
+  lastName: Option[String] = None,
+
+  @(ApiModelProperty @field)(value = "user's twitter id")
+  twitterId: Option[String] = None,
+
+  @(ApiModelProperty @field)(value = "user's linkedin id")
+  linkedinId: Option[String] = None,
+
+  @(ApiModelProperty @field)(value = "user's bio")
+  bio: Option[String] = None,
+
+  @(ApiModelProperty @field)(required = true, value = "user permission id")
+  permissionId: Int,
 
   @(ApiModelProperty @field)(required = true, value = "password of the user")
   password: String )
 
 object UserDto extends DefaultJsonProtocol{
-  implicit val userDtoFormat = jsonFormat4(UserDto.apply)
+  implicit val userDtoFormat = jsonFormat8(UserDto.apply)
 }
